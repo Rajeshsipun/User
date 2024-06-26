3f193cedaec7ddb8b77db26c61652a25b1788482
Rajeshsipun <rkumarbeherarj@gmail.com>
2024-06-26 16:39:46 +0530

    add

--- src/main/java/com/user/user/Entity/PropertyUserEntity.java
+++ src/main/java/com/user/user/Entity/PropertyUserEntity.java
@@ -33,21 +33,26 @@ public class PropertyUserEntity {
         return emailId;
     }
 
+
     public void setEmailId(String emailId) {
         this.emailId = emailId;
     }
+
 
     public String getName() {
         return name;
     }
 
+
     public void setName(String name) {
         this.name = name;
     }
 
+
     public Long getId() {
         return id;
     }
+
 
     public void setId(Long id) {
         this.id = id;
